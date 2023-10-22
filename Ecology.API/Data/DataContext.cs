3372af4379c6a0b45de9cd8fefed78e009a44f35
Crisv19 <125661892+Crisv19@users.noreply.github.com>
2023-10-22 01:19:46 -0500

    migration inicial version0

---
+++ Ecology.API/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Ecology.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecology.API.Data
+{
+    public class DataContext:DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext>options) : base(options) { }
+
+
+        public DbSet<Material>Materials { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Material>().HasIndex(c => c.Name).IsUnique();
+
+        }
+
+    }
+
+}
---
+++ Ecology.Shared/Entities/Material.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecology.Shared.Entities
+{
+    public class Material
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Este campo es requerido")]
+        public string Name { get; set; } = null;
+        
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Description { get; set; } = null;
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Peso { get; set; } = null;
+
+        public string TypMaterial { get; set; } = null; 
+    }
+}
