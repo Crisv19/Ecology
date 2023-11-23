7c5d64912329656855b8cc965dc8f396c65e44ef
Crisv19 <125661892+Crisv19@users.noreply.github.com>
2023-11-23 17:12:38 -0500

    COMMIT LEADER FULL

--- Ecology.API/Controllers/LeadersController.cs
+++ Ecology.API/Controllers/LeadersController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 namespace Market.API.Controllers
 
 {
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
 
     [ApiController]
 
